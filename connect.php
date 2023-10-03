fc079d616547411b686d0aed9992349f645050a9
shrutiiiiin <shrutinaik2317@gmail.com>
2023-10-03 08:28:02 +0530

    seconf]d commit

--- connect.php
+++ connect.php
@@ -5,7 +5,7 @@
   //db connection
   $conn = new mysqli ('localhost', 'root', '', 'test');
   if ($conn->connect_error) {
-    die("Connection failed: " .$conn->connect_error);
+    die("Connection failed: ". $conn->connect_error);
   }
   else
   {
